10ab0f533821c0883bd81a1f15bf2692dec4fe46
simon5155Q <simon5155Q@gmail.com>
2021-01-29 17:28:04 +0530

    added new log

--- sketch.js
+++ sketch.js
@@ -29,6 +29,7 @@ function setup(){
     pig3 = new Pig(810, 220);
 
     log3 =  new Log(810,180,300, PI/2);
+    log6 =  new Log(100,180,300, PI/2);
 
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
@@ -58,6 +59,7 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+    log6.display();
 
     bird.display();
     platform.display();
